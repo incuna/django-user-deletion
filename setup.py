486a3f39160558c85fc12f7bd2264d773ee726b0
Kevin Etienne <kevin@incuna.com>
2016-04-14 15:03:47 +0100

    Add initial project

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+version = '0.1.0'
+
+
+setup(
+    name='django-user-deletion',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    license='BSD',
+    description='Management commands to notify and delete inactive django users',
+    classifiers=(
+        'Development Status :: 1 - Planning',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ),
+    author='Incuna Ltd',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/django-user-deletion',
+)
