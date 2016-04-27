f9f73967f5eac7ec7a34c4e3dc467c57bcb806f9
Kevin Etienne <kevin@incuna.com>
2016-04-27 10:05:10 +0100

    Add test for UserDeletionConfig.MONTHS

--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from io import StringIO
+from unittest.mock import patch
 
 from dateutil.relativedelta import relativedelta
 from django.core import mail
@@ -7,6 +8,9 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from .factories import UserFactory
+
+
+MONTH = 1
 
 
 class TestUserNotifyManagementCommand(TestCase):
@@ -18,15 +22,24 @@ class TestUserNotifyManagementCommand(TestCase):
         self.assertFalse(len(mail.outbox))
 
     def test_inactive_users(self):
-        year_ago = datetime.now() + relativedelta(months=-12)
-        UserFactory.create(last_login=year_ago)
+        month_ago = datetime.now() - relativedelta(months=13)
+        UserFactory.create(last_login=month_ago)
+
+        call_command('notify_users')
+
+        self.assertEqual(len(mail.outbox), 1)
+
+    @patch('user_deletion.apps.UserDeletionConfig.MONTHS', new=MONTH)
+    def test_inactive_users_config(self):
+        month_ago = datetime.now() - relativedelta(months=MONTH + 1)
+        UserFactory.create(last_login=month_ago)
 
         call_command('notify_users')
 
         self.assertEqual(len(mail.outbox), 1)
 
     def test_email(self):
-        year_ago = datetime.now() + relativedelta(months=-12)
+        year_ago = datetime.now() - relativedelta(months=12)
         UserFactory.create(last_login=year_ago)
         call_command('notify_users')
 
@@ -36,7 +49,7 @@ class TestUserNotifyManagementCommand(TestCase):
 
     def test_active_users(self):
         # user was notified before
-        year_ago = datetime.now() + relativedelta(months=-13)
+        year_ago = datetime.now() - relativedelta(months=13)
         UserFactory.create(last_login=year_ago, notified=True)
 
         call_command('notify_users')
