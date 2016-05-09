0ee49cc704a4bf959537751a47498cd04d8cb574
Kevin Etienne <kevin@incuna.com>
2016-05-09 16:53:43 +0100

    Fix app config loaded too early

--- user_deletion/managers.py
+++ user_deletion/managers.py
@@ -1,14 +1,14 @@
 from dateutil.relativedelta import relativedelta
-from django.apps import apps
 from django.utils import timezone
-
-
-user_deletion_config = apps.get_app_config('user_deletion')
 
 
 class UserDeletionManagerMixin:
     def users_to_notify(self):
         """Finds all users who have been inactive and not yet notified."""
+        from django.apps import apps
+
+        user_deletion_config = apps.get_app_config('user_deletion')
+
         threshold = timezone.now() - relativedelta(
             months=user_deletion_config.MONTH_NOTIFICATION,
         )
@@ -16,6 +16,10 @@ class UserDeletionManagerMixin:
 
     def users_to_delete(self):
         """Finds all users who have been inactive and were notified."""
+        from django.apps import apps
+
+        user_deletion_config = apps.get_app_config('user_deletion')
+
         threshold = timezone.now() - relativedelta(
             months=user_deletion_config.MONTH_DELETION,
         )
