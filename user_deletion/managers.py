bf6d6cdaf946af7ce8d1aa6831e7da9b47fef54f
Kevin Etienne <kevin@incuna.com>
2016-05-10 09:01:39 +0100

    Put import back on top

--- user_deletion/managers.py
+++ user_deletion/managers.py
@@ -1,12 +1,11 @@
 from dateutil.relativedelta import relativedelta
+from django.apps import apps
 from django.utils import timezone
 
 
 class UserDeletionManagerMixin:
     def users_to_notify(self):
         """Finds all users who have been inactive and not yet notified."""
-        from django.apps import apps
-
         user_deletion_config = apps.get_app_config('user_deletion')
 
         threshold = timezone.now() - relativedelta(
@@ -16,8 +15,6 @@ class UserDeletionManagerMixin:
 
     def users_to_delete(self):
         """Finds all users who have been inactive and were notified."""
-        from django.apps import apps
-
         user_deletion_config = apps.get_app_config('user_deletion')
 
         threshold = timezone.now() - relativedelta(
